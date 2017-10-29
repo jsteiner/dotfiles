83fc8dd44703cf26f85a1d8b02ed29b4725f2663
Josh Steiner <josh@jsteiner.me>
2017-10-29 09:51:11 -0400

    Fix path for some reason I can't remember

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,3 +1,6 @@
+# mkdir .git/safe in the root of repositories you trust
+PATH=".git/safe/../../bin:.git/safe/../../node_modules/.bin:.git/safe/../../assets/node_modules/.bin:$PATH"
+
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
@@ -5,8 +8,5 @@ PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 if command -v yarn >/dev/null; then
   PATH="$(yarn global bin):$PATH"
 fi
-
-# mkdir .git/safe in the root of repositories you trust
-PATH=".git/safe/../../bin:.git/safe/../../node_modules/.bin:.git/safe/../../assets/node_modules/.bin:$PATH"
 
 export -U PATH
