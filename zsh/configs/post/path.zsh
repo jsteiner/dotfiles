454965437d0b281046ac8eec9deb6cdeb2d6af4b
Josh Steiner <josh@jsteiner.me>
2017-11-27 10:44:44 -0500

    Revert "Fix path for some reason I can't remember"
    This reverts commit 83fc8dd44703cf26f85a1d8b02ed29b4725f2663.

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,6 +1,3 @@
-# mkdir .git/safe in the root of repositories you trust
-PATH=".git/safe/../../bin:.git/safe/../../node_modules/.bin:.git/safe/../../assets/node_modules/.bin:$PATH"
-
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
@@ -8,5 +5,8 @@ PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 if command -v yarn >/dev/null; then
   PATH="$(yarn global bin):$PATH"
 fi
+
+# mkdir .git/safe in the root of repositories you trust
+PATH=".git/safe/../../bin:.git/safe/../../node_modules/.bin:.git/safe/../../assets/node_modules/.bin:$PATH"
 
 export -U PATH
