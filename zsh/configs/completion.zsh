581376baf9c16799f3d8a804475b199ce4273233
Josh Steiner <josh@jsteiner.me>
2019-11-20 19:15:16 -0500

    Fix asdf completions

--- zsh/configs/completion.zsh
+++ zsh/configs/completion.zsh
@@ -1,8 +1,9 @@
-. /usr/local/etc/bash_completion.d/git-completion.bash 2> /dev/null
-
 # load our own completion functions
 fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 
 # completion
 autoload -U compinit
 compinit
+
+autoload bashcompinit
+bashcompinit
